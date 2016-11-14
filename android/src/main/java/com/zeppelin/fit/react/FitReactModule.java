f7da927629afd77e65455093a04c70bfa6373570
mcmhav <mcmhav@gmail.com>
2016-11-14 09:51:52 +0100

    add better check for initFitKit on android

--- android/src/main/java/com/zeppelin/fit/react/FitReactModule.java
+++ android/src/main/java/com/zeppelin/fit/react/FitReactModule.java
@@ -1,94 +1,37 @@
 package com.zeppelin.fit.react;
 
 import android.content.Context;
-import com.facebook.react.bridge.WritableArray;
-import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.Promise;
-import com.facebook.react.bridge.Arguments;
 
-// ^ is cleaned
-
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-
-import com.facebook.react.bridge.Callback; // TODO: remove
-
-import com.facebook.react.bridge.JSApplicationCausedNativeException;
-
-import android.app.Activity;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.app.ActivityCompat;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.util.Log;
-import android.R;
-import android.view.View;
-import android.os.Bundle;
 
-import android.content.Intent;
 
-// permission
-import android.support.design.widget.Snackbar;
-
-// Fitness related:
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.fitness.Fitness;
-import com.google.android.gms.common.Scopes;
-import com.google.android.gms.common.api.Scope;
-
-import android.os.Bundle;
-import android.content.IntentSender;
-
-// read session related:
-import com.google.android.gms.fitness.request.SessionReadRequest;
-import com.google.android.gms.fitness.data.Session;
-import java.util.Calendar;
-import android.os.AsyncTask;
-import com.google.android.gms.fitness.result.SessionReadResult;
+// date related:
 import java.util.Date;
-import com.google.android.gms.fitness.data.DataType;
 import java.text.SimpleDateFormat;
-import java.util.Map;
-import java.util.HashMap;
-// import java.time.format.DateTimeFormatter;
-import java.util.List;
-import com.google.android.gms.fitness.data.DataPoint;
-
-// Log related:
 import java.text.DateFormat;
-import com.google.android.gms.fitness.data.Field;
-import java.util.concurrent.TimeUnit;
-import com.google.android.gms.fitness.data.DataSet;
-import static java.text.DateFormat.getTimeInstance;
-
-
-// bodymetrics
-// import rx.Single;
-
 import java.sql.Timestamp;
 import java.util.TimeZone;
 
-// background service
+// background service:
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.app.job.JobParameters;
 import android.os.Messenger;
 import android.os.Message;
 import android.os.Handler;
 import android.os.Looper;
-
-import android.app.job.JobScheduler;
 import android.app.job.JobInfo;
 import android.content.ComponentName;
-import android.content.Context;
-import android.widget.Toast;
 
+// ?
+import android.app.job.JobScheduler;
+
+// FitKit:
 import com.zeppelin.fit.react.FitJobService;
 import com.zeppelin.fit.react.FitBodyMetricsService;
 import com.zeppelin.fit.react.FitActivitiesService;
@@ -109,23 +52,12 @@ class FitReactModule extends ReactContextBaseJavaModule {
 
     private final static String REACT_MODULE_NAME = "FitKit";
     public static final String TAG = "RCTFitKit";
-    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
 
-    static final int FIT_API_REQUEST_CODE = 4;
-    static final int RESULT_OK = 100;
-
-    public static final int MSG_SERVICE_OBJ = 2;
     public static final int MSG_UNCOLOUR_START = 0;
     public static final int MSG_UNCOLOUR_STOP = 1;
-
-    private GoogleApiClient mClient = null;
-    private boolean authInProgress = false;
-
-    private Promise getActivitiesPromise = null;
+    public static final int MSG_SERVICE_OBJ = 2;
 
     private RxFit rxFit;
-
-    private Map<String, String> googleFitToFitKitActivityMap = new HashMap<String, String>();
 
     public FitReactModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -164,19 +96,34 @@ class FitReactModule extends ReactContextBaseJavaModule {
                     new Scope(Scopes.FITNESS_BODY_READ)
                 }
             );
-            // Completable lol = rxFit.checkConnection();
+            Completable rxFitConnection = rxFit.checkConnection();
 
-            // lol.fromAction(new Action0() {
-            //     @Override
-            //     public void call() {
-            //         Log.i(TAG, "doOnCompleted");
-            //         promise.resolve(true);
-            //     }
-            // }).subscribe();
-            promise.resolve(true);
+            Log.i(TAG, "check connection");
+
+            rxFitConnection
+                .doOnCompleted(new Action0() {
+                    @Override
+                    public void call() {
+                        Log.i(TAG, "success!!!!");
+                        promise.resolve(true);
+                    }
+                })
+                .doOnError(new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable error) {
+                        Log.e(TAG, "error!!!!");
+                        error.printStackTrace();
+                        promise.reject("could not init fit kit", error);
+                    }
+                })
+                .onErrorComplete()
+                .subscribe();
+
         } catch(Exception e) {
+            rxFit = null;
+            Log.i(TAG, "error setting up Google Fit connection");
             e.printStackTrace();
-            promise.reject("could not init fit kit");
+            promise.reject("could not init fit kit", e);
         }
     }
 
