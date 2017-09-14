3d6615f4d2d09d5c151d5a25c15b0dcbc8bb8ed6
mcmhav <mcmhav@gmail.com>
2017-09-14 16:21:32 +0200

    remove deprecated override

--- android/src/main/java/com/zeppelin/fit/react/FitReactPackage.java
+++ android/src/main/java/com/zeppelin/fit/react/FitReactPackage.java
@@ -22,7 +22,8 @@ public class FitReactPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
+    // Depreciated RN 0.47
+    // @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
